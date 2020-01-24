c6a2aaaf5fcda64547e9f9463940ccb77a690195
Трубкин Сергей <trubkinsa@gmail.com>
2020-01-24 12:47:25 +0300

    Устраняет замечания после первой проверки проекта

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,11 @@
-var toggle = document.querySelector(".main-nav__button");
+var toggle = document.querySelector(".page-header__button");
 var mainNav = document.querySelector(".main-nav");
 
 mainNav.classList.remove("main-nav--no-js");
+toggle.classList.remove("page-header__button--no-js")
 
 toggle.addEventListener("click", function (evt) {
   evt.preventDefault();
-  toggle.classList.toggle("main-nav__button--close");
+  toggle.classList.toggle("page-header__button--close");
   mainNav.classList.toggle("main-nav--open");
 })
